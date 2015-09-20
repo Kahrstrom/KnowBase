12d22ecd964de95a4a1e554c20d2eb5dedc62cd0
Jonatan Kåhrström <jonatan.kahrstrom@gmail.com>
2015-09-20 16:59:45 +0200

    postgress added! Woo

--- app/app.py
+++ app/app.py
@@ -1,21 +1,46 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, abort
 
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello():
-	return 'Hello, World!'
+educations = [
+	{
+		'ideducation' : 1,
+		'title' : 'Supertitel',
+		'school' : 'Superskolan',
+		'enddate' : '2015-01-01',
+		'description' : 'Shit vilken superutbildning det var!',
+		'user' : 1
+	},
+	{
+		'ideducation' : 2,
+		'title' : 'Supertitel2',
+		'school' : 'Superskolan2',
+		'enddate' : '2015-01-01',
+		'description' : 'Shit vilken superutbildning det var igen!',
+		'user' : 1
+	},
+	{
+		'ideducation' : 3,
+		'title' : 'Kass titel',
+		'school' : 'Usel skola',
+		'enddate' : '',
+		'description' : 'urgh...!',
+		'user' : 2
+	}
+]
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-	if request.method == 'POST':
-		value_one = int(request.form.get('first'))
-		value_two = int(request.form.get('second'))
-		total = value_one + value_two
-		data = {"total" : str(total)}
-		return jsonify(data)
-	return render_template('index.html')
+@app.route('/knowledge/api/educations', methods=['GET'])
+def get_educations():
+	if len(educations) == 0:
+		abort(404)
+	return jsonify({'educations' : educations})
 
+@app.route('/knowledge/api/educations/<int:ideducation>')
+def get_education(ideducation):
+	education = [education for education in educations if education['ideducation'] == ideducation]
+	if len(education) == 0:
+		abort(404)
+	return jsonify({'education' : education})
 
 if __name__ == '__main__':
 	app.run(debug=True)
---
+++ database/create_table_education.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE education(
+ ideducation BIGSERIAL PRIMARY KEY NOT NULL,
+ title CHAR(64) NOT NULL,
+ school CHAR(32) NOT NULL,
+ enddate  DATE,
+ description CHAR(512) NOT NULL,
+ "user" INT,
+ timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ createdtime  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+ createduser INT,
+ updateduser  INT
+)
